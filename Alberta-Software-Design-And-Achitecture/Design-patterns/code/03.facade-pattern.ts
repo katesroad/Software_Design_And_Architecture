c53228cd8adce3a769c8458d1ae0dffee2c23151
kate <cat.katecc@gmail.com>
2020-11-22 15:43:06 -0500

    facade pattern

--- Alberta-Software-Design-And-Achitecture/Design-patterns/code/03.facade-pattern.ts
+++ Alberta-Software-Design-And-Achitecture/Design-patterns/code/03.facade-pattern.ts
@@ -5,13 +5,59 @@ interface IAccount {
   getAccountNumber(): number;
 }
 
-class Chequing implements IAccount { }
+class Chequing implements IAccount {
+  private amount = 0;
+  constructor(initAmount: number, private readonly number: number) {
+    this.amount = initAmount;
+  }
+
+  deposit(amount: number) {
+    if (amount <= 0) return;
+    this.amount += amount;
+  }
+
+  withdraw(amount: number) {
+    if (this.amount - amount >= 0) {
+      this, amount -= amount;
+    } else {
+      throw new Error('')
+    }
+  }
+
+  getAccountNumber() {
+    return this.number;
+  }
+
+  transfer(amount: number) {
+    if (this.amount - amount >= 0) {
+      this, amount -= amount;
+    } else {
+      throw new Error('')
+    }
+  }
+}
+
 class Saving implements IAccount { }
 class Investment implements IAccount { }
 
 // facade class is a wrapper class
 
+class HashTable<T1, T> {
+  private id: T1;
+  private account: T;
+}
 class BankService {
-  constructor(private check: Chequing, private saving: Saving, private investment: Investment) { }
-  
+  private bankAccounts: HashTable<number, IAccount>;
+
+  public BanService() {
+    this.bankAccounts = new HashTable<init, IAccount>;
+  }
+
+  public createAccount(type: string, initAmount: number) {
+    let account: IAccount = null;
+    switch (type) {
+      case 'chequing':
+        account = new Chequing(initAmount);
+    }
+  }
 }
